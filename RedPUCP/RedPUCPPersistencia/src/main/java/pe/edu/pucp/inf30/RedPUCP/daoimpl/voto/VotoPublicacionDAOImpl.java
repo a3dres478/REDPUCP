ac9ed455a4c76035ce312e338f59b8486cae36a3
a3dres478 <163962856+a3dres478@users.noreply.github.com>
2025-10-03 12:11:59 -0500

    agregacion de negocio
    se agrego negocioparte

---
+++ RedPUCP/RedPUCPPersistencia/src/main/java/pe/edu/pucp/inf30/RedPUCP/daoimpl/voto/VotoPublicacionDAOImpl.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.edu.pucp.inf30.RedPUCP.daoimpl.voto;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+import pe.edu.pucp.inf30.RedPUCP.dao.voto.VotoPublicacionDAO;
+import pe.edu.pucp.inf30.RedPUCP.daoimpl.BaseDAOImplement;
+import pe.edu.pucp.inf30.RedPUCP.daoimpl.usuario.UsuarioDAOimpl;
+//import pe.edu.pucp.inf30.RedPUCP.modelo.voto.Voto;
+import pe.edu.pucp.inf30.RedPUCP.modelo.voto.VotoPublicacion;
+/**
+ *
+ * @author andre
+ */
+public class VotoPublicacionDAOImpl extends BaseDAOImplement<VotoPublicacion> implements VotoPublicacionDAO{
+    
+    @Override
+    protected CallableStatement comandoInsertar(Connection conn, VotoPublicacion usu) throws SQLException {
+        String sql = "{CALL sp_crearVotoPublicacion(?,?,?)}";
+        CallableStatement cmd = conn.prepareCall(sql);
+        cmd.setInt("p_idUsuario", usu.getUsuario().getIdUsuario());
+        //cmd.setString("p_tipo", String.valueOf(usu.getTipo()));
+        cmd.setString("p_tipo", "UP");
+        //cmd.setDate("p_fechaRegistro", new java.sql.Date(usu.getFechaRegistro().getTime()));
+        cmd.registerOutParameter("p_id", Types.INTEGER);
+        
+        
+        return cmd;
+    }
+
+    @Override
+    protected CallableStatement comandoModificar(Connection conn, VotoPublicacion usu) throws SQLException {
+        String sql = "{CALL sp_actualizarVotoPublicacion(?,?,?)}";
+        CallableStatement cmd = conn.prepareCall(sql);
+        //cmd.setInt("p_idUsuario", usu.getUsuario().getIdUsuario());
+        cmd.setInt("p_idVoto", usu.getId());
+        //cmd.setString("p_tipo", String.valueOf(usu.getTipo()));
+        cmd.setString("p_tipo", "DOWN");
+        //cmd.setDate("p_fechaRegistro", new java.sql.Date(usu.getFechaRegistro().getTime()));
+        cmd.registerOutParameter("p_exito", Types.BOOLEAN);
+        return cmd;
+    }
+
+
+    @Override
+    protected CallableStatement comandoEliminar(Connection conn, int id) throws SQLException {
+        String sql = "{CALL sp_eliminarVoto(?,?)}";
+        CallableStatement cmd = conn.prepareCall(sql);
+        cmd.setInt("p_idVoto", id);
+        cmd.registerOutParameter("p_exito", Types.BOOLEAN);
+        return cmd;
+    }
+
+
+    @Override
+    protected CallableStatement comandoBuscar(Connection conn, int id) throws SQLException {
+        String sql = "{CALL sp_obtenerVotoPorId(?)}";
+        CallableStatement cmd = conn.prepareCall(sql);
+        cmd.setInt("p_idVoto", id);
+        return cmd;
+    }
+
+
+    @Override
+    protected CallableStatement comandoListar(Connection conn) throws SQLException {
+        String sql = "{CALL sp_listarVotos()}";
+        CallableStatement cmd = conn.prepareCall(sql);
+        return cmd;
+    }
+
+    @Override
+    protected VotoPublicacion mapearModelo(ResultSet rs) throws SQLException {
+        VotoPublicacion usu = new VotoPublicacion();
+        usu.setId(rs.getInt("idVoto"));
+        usu.setUsuario(new UsuarioDAOimpl().buscar(rs.getInt("idUsuario")));
+        usu.setFechaRegistro(rs.getTimestamp("fechaRegistro"));
+        usu.setTipo(rs.getString("tipo").charAt(0));
+        
+        
+        return usu;
+    }
+    
+}
