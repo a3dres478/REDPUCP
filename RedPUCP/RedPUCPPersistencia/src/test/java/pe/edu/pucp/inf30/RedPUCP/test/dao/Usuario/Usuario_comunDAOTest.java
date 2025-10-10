7e0202202f24c99a76d7271e21fda0546ccdea5d
a3dres478 <163962856+a3dres478@users.noreply.github.com>
2025-10-09 20:28:05 -0500

    USUARIOTEST
    se agrego los test dao de usuario comun y de administrador

---
+++ RedPUCP/RedPUCPPersistencia/src/test/java/pe/edu/pucp/inf30/RedPUCP/test/dao/Usuario/Usuario_comunDAOTest.java
@@ -0,0 +1,191 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.edu.pucp.inf30.RedPUCP.test.dao.Usuario;
+
+
+import java.time.Instant;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import pe.edu.pucp.inf30.RedPUCP.test.dao.PersistibleProbable;
+
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import pe.edu.pucp.inf30.RedPUCP.dao.Comunidad.ComunidadDAO;
+import pe.edu.pucp.inf30.RedPUCP.dao.usuario.UsuarioDAO;
+import pe.edu.pucp.inf30.RedPUCP.dao.usuario.Usuario_comunDAO;
+import pe.edu.pucp.inf30.RedPUCP.daoimpl.Comunidad.ComunidadDAOimpl;
+import pe.edu.pucp.inf30.RedPUCP.daoimpl.usuario.UsuarioDAOimpl;
+import pe.edu.pucp.inf30.RedPUCP.daoimpl.usuario.Usuario_comunDAOimpl;
+import pe.edu.pucp.inf30.RedPUCP.modelo.Comunidad.Comunidad;
+import pe.edu.pucp.inf30.RedPUCP.modelo.Comunidad.EstadoComunidad;
+import pe.edu.pucp.inf30.RedPUCP.modelo.usuario.Usuario;
+import pe.edu.pucp.inf30.RedPUCP.modelo.usuario.Usuario_comun;
+
+
+/**
+ *
+ * @author andre
+ */
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+
+public class Usuario_comunDAOTest implements  PersistibleProbable{
+    private int testId;
+    private int testUsuarioId;
+    private final int idIncorrecto = 999999;
+
+//    @BeforeAll
+//    public void inicializar() {
+//        Usuario_comunDAO usuarioComunDAO = new Usuario_comunDAOimpl();
+//        Usuario_comun usuarioComun = new Usuario_comun();
+//        usuarioComun.setNombre("Usuario -> ComunidadTest");
+//        usuarioComun.setDescripcion("Usuario de Prueba para Comunidad");
+//        this.testUsuarioId = usuarioComunDAO.crear(usuarioComun);
+//    }
+//    
+//    @AfterAll
+//    public void limpiar() {
+//        Usuario_comunDAO usuarioComunDAO = new Usuario_comunDAOimpl();
+//        usuarioComunDAO.eliminar(testUsuarioId);
+//    }
+    
+    @Test
+    @Order(1)
+    @Override
+    public void debeCrear(){
+       // Date fechaactual= new Date(System.currentTimeMillis());
+        Usuario_comunDAO usuariocomunDAO=new Usuario_comunDAOimpl();
+        Usuario_comun comun= new Usuario_comun();
+        comun.setCodigopucp("1234");
+        comun.setContrasenha("1234");
+        comun.setDescripcion("Usuario_comunDes1");
+        comun.setNombre("Usuario_comun1");
+        comun.setEmail("correo@gmail.com");
+        comun.setFechaRegistro(new java.util.Date());
+        comun.setEstadouser('A');
+        comun.setKarma(0);
+        this.testId=usuariocomunDAO.crear(comun);
+        assertTrue(this.testId>0);
+    }
+    
+    @Test
+    @Order(2)
+    @Override
+    public void debeActualizarSiIdExiste() {
+        Usuario_comunDAO usuariocomunDAO=new Usuario_comunDAOimpl();
+        Usuario_comun comun= new Usuario_comun();
+        
+        comun.setIdUsuario(this.testId);
+        comun.setNombre("Usuario_comun2");
+        comun.setCodigopucp("1234");
+        comun.setDescripcion("Usuario_comunDes1");
+        comun.setKarma(0);
+        comun.setEstadouser('A');
+        comun.setEmail("correo@gmail");
+        comun.setFechaRegistro(new java.util.Date());
+        boolean modifico=usuariocomunDAO.actualizar(comun);
+        assertTrue(modifico);
+        Usuario_comun usuariocomun=usuariocomunDAO.leer(this.testId);
+        assertEquals(usuariocomun.getNombre(),"Usuario_comun2");
+        assertEquals(usuariocomun.getDescripcion(),"Usuario_comunDes1");
+        assertEquals(usuariocomun.getEstadouser(), 'S');
+        assertEquals(usuariocomun.getCodigopucp(),"1234");
+    }
+    
+    @Test
+    @Order(3)
+    @Override
+    public void noDebeActualizarSiIdNoExiste() {
+        Usuario_comunDAO usuariocomunDAO=new Usuario_comunDAOimpl();
+        Usuario_comun comun= new Usuario_comun();
+        comun.setIdUsuario(this.idIncorrecto);
+        comun.setNombre("Usuario_comun3");
+        comun.setCodigopucp("1234");
+        comun.setDescripcion("Usuario_comunDes3");
+        comun.setKarma(0);
+        comun.setEstadouser('S');
+        comun.setEmail("correo@gmail");
+        comun.setFechaRegistro(new java.util.Date());
+        
+        boolean modifico=usuariocomunDAO.actualizar(comun);
+        assertTrue(modifico);
+        Usuario_comun usuariocomun=usuariocomunDAO.leer(this.testId);
+        assertEquals(usuariocomun.getNombre(),"Usuario_comun2");
+        assertEquals(usuariocomun.getDescripcion(),"Usuario_comunDes1");
+        assertEquals(usuariocomun.getEstadouser(), 'S');
+        assertEquals(usuariocomun.getCodigopucp(),"1234");
+        
+        assertFalse(modifico);
+    }
+    
+    @Test
+    @Order(4)
+    @Override
+    public void noDebeEliminarSiIdNoExiste() {
+        Usuario_comunDAO usuarioDAO=new Usuario_comunDAOimpl();
+        boolean elimino =usuarioDAO.eliminar(this.idIncorrecto);
+        assertFalse(elimino);
+        
+        
+        
+        
+    }
+    
+    @Test
+    @Order(5)
+    @Override
+    public void debeLeerSiIdExiste() {
+        Usuario_comunDAO usuarioDAO=new Usuario_comunDAOimpl();
+        Usuario_comun comun= usuarioDAO.leer(this.testId);
+        assertNotNull(comun);
+    }
+    
+    @Test
+    @Order(6)
+    @Override
+    public void noDebeLeerSiIdNoExiste() {
+        Usuario_comunDAO usuarioDAO=new Usuario_comunDAOimpl();
+        Usuario_comun comun= usuarioDAO.leer(this.idIncorrecto);
+        assertNull(comun);
+    }
+    
+    @Test
+    @Order(7)
+    @Override
+    public void debeLeerTodos() {
+        Usuario_comunDAO usuarioDAO= new Usuario_comunDAOimpl();
+        List<Usuario_comun> usuarios =usuarioDAO.leerTodos();
+        assertNotNull(usuarios);
+        assertFalse(usuarios.isEmpty());
+    }
+    
+    @Test
+    @Order(8)
+    @Override
+    public void debeEliminarSiIdExiste() {
+        Usuario_comunDAO usuarioDAO= new Usuario_comunDAOimpl();
+        boolean elimino = usuarioDAO.eliminar(this.testId);
+        assertTrue(elimino); 
+    }
+
+
+}
