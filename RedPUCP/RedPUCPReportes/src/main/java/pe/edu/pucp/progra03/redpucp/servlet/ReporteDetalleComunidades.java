c6d45bf56c5c6f9982260433af440bb0ffb8bdb1
a3dres478 <163962856+a3dres478@users.noreply.github.com>
2025-11-16 00:16:42 -0500

    ultimos reporets hechos

--- RedPUCP/RedPUCPReportes/src/main/java/pe/edu/pucp/progra03/redpucp/servlet/ReporteDetalleComunidades.java
+++ RedPUCP/RedPUCPReportes/src/main/java/pe/edu/pucp/progra03/redpucp/servlet/ReporteDetalleComunidades.java
@@ -1,51 +1,44 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
 package pe.edu.pucp.progra03.redpucp.servlet;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.awt.Image;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.jar.JarException;
-import javax.imageio.ImageIO;
-import pe.edu.pucp.inf30.RedPUCP.config.DBManager;
-
-
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
+import pe.edu.pucp.inf30.RedPUCP.config.DBManager;
 
 /**
  *
  * @author andre
  */
-@WebServlet(name = "ReporteUsuariosTOP", 
-        urlPatterns = {"/reportes/usuarios"})
-public class ReporteUsuariosTOP extends HttpServlet {
+//este se encarga de obtener las publicaciones por x usuario
+@WebServlet(name = "ReporteDetalleComunidades", urlPatterns = {"/reportes/ReporteDetalleComunidades"})
+public class ReporteDetalleComunidades extends HttpServlet {
+
     private final String NOMBRE_REPORTE = 
-            " .jasper";
-    private final String NOMBRE_LOGO = 
-            ".png";
-    //ups wir haben keinen
+            "reportes/PublicacionesxUsuario.jasper";
     
     @Override
     protected void doGet(
-            HttpServletRequest request, 
+            HttpServletRequest request,
             HttpServletResponse response)
             throws ServletException, IOException {
-        
         response.setContentType("application/pdf");
         
         try {
@@ -53,26 +46,16 @@ public class ReporteUsuariosTOP extends HttpServlet {
                     getClass().getClassLoader()
                             .getResourceAsStream(
                                     this.NOMBRE_REPORTE);
-            
+
             if (reporte == null) {
-                throw new FileNotFoundException("No se encontró el archivo 'UsuariosTOP.jasper'");
+                throw new FileNotFoundException("No se encontró el archivo 'Cursosxprofesores.jasper'");
             }
+
+            Map<String, Object> parametros = new HashMap<>();
+            int id = Integer.parseInt(request.getParameter("id"));
             
-            Map<String, Object> parametros = 
-                    new HashMap<>();
-//            int id = Integer.parseInt(request.getParameter("id"));
-//            parametros.put("idAlumno", id);
-            parametros.put("estado", request.getParameter("est"));
-            //parametros.put("fecha_inicio", java.sql.Date.valueOf(request.getParameter("fi")));
-            //parametros.put("fecha_fin", java.sql.Date.valueOf(request.getParameter("ff")));
-            
-            InputStream logoStream = getClass().getClassLoader().
-                    getResourceAsStream(this.NOMBRE_LOGO);
-            if (logoStream != null) {
-                Image logo = ImageIO.read(logoStream);
-                parametros.put("Logo", logo);
-            }
-            
+            parametros.put("IDUSUARIO", id);
+            // JasperPrint jp = JasperFillManager.fillReport(jr, parametros, new JREmptyDataSource());
             try (Connection conexion = DBManager.getInstance().getConnection()) {
                 JasperPrint jp = 
                         JasperFillManager.fillReport(reporte, 
@@ -92,11 +75,9 @@ public class ReporteUsuariosTOP extends HttpServlet {
         }
     }
 
+    
     @Override
     public String getServletInfo() {
-        return "Genera reporte de Usuarios top";
+        return "Esta servlet genera el reporte de cursos por profesores.";
     }
-    
-    
-    
 }
--- RedPUCP/RedPUCPReportes/src/main/java/pe/edu/pucp/progra03/redpucp/servlet/ReporteListarComunidades.java
+++ RedPUCP/RedPUCPReportes/src/main/java/pe/edu/pucp/progra03/redpucp/servlet/ReporteListarComunidades.java
@@ -1,9 +1,11 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
 package pe.edu.pucp.progra03.redpucp.servlet;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -11,34 +13,32 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.awt.Image;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.jar.JarException;
 import javax.imageio.ImageIO;
-import pe.edu.pucp.inf30.RedPUCP.config.DBManager;
-
-
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
+import pe.edu.pucp.inf30.RedPUCP.config.DBManager;
 
 /**
  *
  * @author andre
  */
-@WebServlet(name = "ReporteUsuariosTOP", 
-        urlPatterns = {"/reportes/usuarios"})
-public class ReporteUsuariosTOP extends HttpServlet {
+//Obtinee la inforamcion general de las comunidades
+@WebServlet(name = "ReporteListarComunidades", urlPatterns = {"/reportes/ReporteListarComunidades"})
+public class ReporteListarComunidades extends HttpServlet {
+
     private final String NOMBRE_REPORTE = 
             " .jasper";
     private final String NOMBRE_LOGO = 
             ".png";
     //ups wir haben keinen
+    
     
     @Override
     protected void doGet(
@@ -55,16 +55,13 @@ public class ReporteUsuariosTOP extends HttpServlet {
                                     this.NOMBRE_REPORTE);
             
             if (reporte == null) {
-                throw new FileNotFoundException("No se encontró el archivo 'UsuariosTOP.jasper'");
+                throw new FileNotFoundException("No se encontró el archivo 'InfoComunidades.jasper'");
             }
             
             Map<String, Object> parametros = 
                     new HashMap<>();
-//            int id = Integer.parseInt(request.getParameter("id"));
-//            parametros.put("idAlumno", id);
             parametros.put("estado", request.getParameter("est"));
-            //parametros.put("fecha_inicio", java.sql.Date.valueOf(request.getParameter("fi")));
-            //parametros.put("fecha_fin", java.sql.Date.valueOf(request.getParameter("ff")));
+            
             
             InputStream logoStream = getClass().getClassLoader().
                     getResourceAsStream(this.NOMBRE_LOGO);
@@ -94,9 +91,8 @@ public class ReporteUsuariosTOP extends HttpServlet {
 
     @Override
     public String getServletInfo() {
-        return "Genera reporte de Usuarios top";
+        return "Genera reporte de ListadoInfoComunidades";
     }
     
-    
-    
+
 }
